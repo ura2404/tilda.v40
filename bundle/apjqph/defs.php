42e6bddcfea5b957bc0efa3d4c0758c216c65040
ura <ura@itx.ru>
2021-11-30 07:41:16 +0300

    .

--- bundle/apjqph/defs.php
+++ bundle/apjqph/defs.php
@@ -7,7 +7,7 @@ define('CM_MODE',PHP_SAPI === 'cli' ? 'development' :(isset($_SERVER['CM_MODE'])
 
 if(PHP_SAPI !== 'cli'){
     $Config = json_decode(file_get_contents(CM_TOP.'/config.json'),true);
-    if(!$Config || !isset($Config['www']) || !isset($Config['www']['root'])) die('Can-t define "CM_WHOM". Wrong config file!');
+    if(!$Config || !isset($Config['www']) || !isset($Config['www']['root'])) die('Can\'t define "CM_WHOME". Wrong config file!');
     define('CM_WHOME',$_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['SERVER_NAME'] .':'. $_SERVER['SERVER_PORT']. $Config['www']['root']);
 }
 ?>
